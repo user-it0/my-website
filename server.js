46f32b5550abf6d6be4043a4eab961629db77353
user-it0 <function.paradox@gmail.com>
2025-03-07 21:45:13 +0900

    web massage

--- server.js
+++ server.js
@@ -38,4 +38,5 @@ io.on("connection", socket => {
   });
 });
 
-server.listen(3000, () => console.log("Server running on port 3000"));
+const PORT = process.env.PORT || 5000;
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
